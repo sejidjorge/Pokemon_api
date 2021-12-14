b50b074862188b42f2fb2bb01ebad0c925c10621
Jorge <sejidjorge@gmail.com>
2021-12-14 11:41:59 -0300

    grid card results

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,41 +6,52 @@ export default function UserCard(){
 
     const [next, setNext] = useState(0)
 
-    const [listPokemon, setlistPokemon] = useState(null)
+    const [skill, setSkill] = useState(null)
 
-    // useEffect(() => {
-    //     axios.get(`https://pokeapi.co/api/v2/pokemon/${search}/`).then(function(response){
-    // })
-    // }, [search])
+    const selectPokemon = (pokemon) => {
+        if (pokemon){
+            axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then((response) => {
+            setSkill([...response.data?.abilities])
+        })
+        }
+    }
+
+    const [listPokemon, setlistPokemon] = useState(null)
 
     useEffect(() => {
 
         axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${next}`).then((response) =>{
             setlistPokemon([...response.data?.results])
     })
-    }, [next])
-    
+    }, [next]);
+
     return(
-    <><div className="flex items-center justify-center">
-            <div className="grid grid-cols-5 gap-4">
-                {listPokemon?.map((value, index) => (
-                    <div>
-                        <Card>
-                            <h1 key={index}>Pokemon</h1>
-                            <p>Nome: {value.name}</p>
-                        </Card>
-                    </div>
-                ))}
+    <><div className="container">
+        <div className="flex justify-center items-center">
+                <div className="grid grid-cols-4 gap-4">
+                    {listPokemon?.map((value, index) => (
+                        <div>
+                            <Card key={index}>
+                                <h1 className="text-red-900">Pokemon</h1>
+                                <p>Nome: {value.name}</p>
+                                {skill?.map((value) => (
+                                <p>Habilidade: {value.nome}</p>
+                                ))}
+                                <Button className="m-4" onClick={() => selectPokemon(value.name)} variant="contained">
+                                    Ver mais
+                                </Button>
+                            </Card>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
-        <div className="flex items-center justify-center">
             <div className="flex justify-around m-4">
-                    <Button className="m-4" onClick={() => setNext(next > 0 && next - 20)} variant="contained">
-                        Anterior
-                    </Button>
-                    <Button className="m-4" onClick={() => setNext(next + 20)} variant="contained">
-                        Proximo
-                    </Button>
+                <Button className="m-4" onClick={() => setNext(next > 0 && next - 20)} variant="contained">
+                    Anterior
+                </Button>
+                <Button className="m-4" onClick={() => setNext(next + 20)} variant="contained">
+                    Proximo
+                </Button>
             </div>
         </div></>
     );
