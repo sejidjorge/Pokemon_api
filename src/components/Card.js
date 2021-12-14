08159fdf89f49bf49a109a2f4dcf312a7ac31206
Jorge <sejidjorge@gmail.com>
2021-12-14 17:14:19 -0300

    cards pokemon

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,66 +4,54 @@ import { Button, Card, CardMedia, Typography } from "@mui/material";
 
 export default function UserCard(){
 
-    const [next, setNext] = useState(0)
+    const [next, setNext] = useState(1)
 
     const [pokemon, setPokemon] = useState(null)
 
-    const selectPokemon = (pokemon) => {
-        if (pokemon){
-            axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then((response) => {
-            setPokemon({...response.data})
-        })
-        }
-    }
-
-    const [listPokemon, setlistPokemon] = useState(null)
-
     useEffect(() => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${next}`).then((response) => {
+        setPokemon({...response.data})
 
-        axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${next}`).then((response) =>{
-            setlistPokemon([...response.data?.results])
-    })
+        })
     }, [next]);
 
     return(
     <>
         <div className="container">
             <div className="flex justify-center mt-4">
-                    <div className="grid grid-cols-5 gap-4">
-                        {listPokemon?.map((value, index) => (
+                    <div className="grid grid-cols-1 gap-1">
                             <div>
-                                <Card key={index} className="min-h-min items-center" style={{ width: '12vw'}}>
-                                    <Typography gutterBottom variant="h5" component="div">Pokemon</Typography>
-                                    <Typography gutterBottom variant="p" component="div" >Nome: {value.name}</Typography>
-                                    {value?.name === pokemon?.name ? 
+                                <Card className="min-h-min" style={{ width: '32vw'}}>
+                                    <Typography gutterBottom variant="h5" component="div">{pokemon?.name}</Typography>
+                                    <div>
+                                    <Typography gutterBottom variant="p" component="div">Habilidades: </Typography>
+                                    {pokemon?.name && 
                                         pokemon?.abilities.map((ability, index) => (
-                                            <Typography gutterBottom variant="p" component="div" key={index}>{index+1+"°"} Habilidade: {ability.ability.name}</Typography>
+                                            <><Typography className="text-sm" gutterBottom variant="p" component="div" key={index}>{ability.ability.name}</Typography></>
                                             ))
-                                    : null}
-                                    {value?.name === pokemon?.name ? (
+                                    }
+                                    </div>
+                                    <div>
+                                    </div>
+                                    {pokemon?.name && 
                                         <CardMedia
                                         component="img"
                                         style={{width: '100%'}}
                                         image={pokemon?.sprites?.other["official-artwork"].front_default}
                                         alt=""
                                             />
-                                    ) : null}
-                                        <Button className="m-4" onClick={() => selectPokemon(value.name)} variant="contained" size="small">
-                                        Ver mais
-                                        </Button>
-                                    
+                                    }
                                 </Card>
                             </div>
-                        ))}
                     </div>
                 </div>
                 <div className="flex justify-around m-4">
-                    {next > 0 && (
-                        <Button className="m-4" onClick={() => setNext(next > 0 && next - 10)} variant="contained">
+                    {next > 1 && (
+                        <Button className="m-4" onClick={() => setNext(next > 1 && next - 1)} variant="contained">
                         Anterior
                         </Button>
                     )}
-                    <Button className="m-4" onClick={() => setNext(next + 10)} variant="contained">
+                    <Button className="m-4" onClick={() => setNext(next + 1)} variant="contained">
                         Proximo
                     </Button>
                 </div>
