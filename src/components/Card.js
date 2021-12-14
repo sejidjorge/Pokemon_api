b01947f653ca4bc6574f942e66064da4f0cd86d7
Jorge <sejidjorge@gmail.com>
2021-12-14 13:42:48 -0300

    layout habilidades

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-import { Button, Card } from "@mui/material";
+import { Button, Card, Typography } from "@mui/material";
 
 export default function UserCard(){
 
     const [next, setNext] = useState(0)
 
-    const [skill, setSkill] = useState(null)
+    const [pokemon, setPokemon] = useState(null)
 
     const selectPokemon = (pokemon) => {
         if (pokemon){
             axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then((response) => {
-            setSkill([...response.data?.abilities])
+            setPokemon({...response.data})
         })
         }
     }
@@ -26,34 +26,41 @@ export default function UserCard(){
     }, [next]);
 
     return(
-    <><div className="container">
-        <div className="flex justify-center items-center">
-                <div className="grid grid-cols-4 gap-4">
-                    {listPokemon?.map((value, index) => (
-                        <div>
-                            <Card key={index}>
-                                <h1 className="text-red-900">Pokemon</h1>
-                                <p>Nome: {value.name}</p>
-                                {skill?.map((value) => (
-                                <p>Habilidade: {value.nome}</p>
-                                ))}
-                                <Button className="m-4" onClick={() => selectPokemon(value.name)} variant="contained">
-                                    Ver mais
-                                </Button>
-                            </Card>
-                        </div>
-                    ))}
+    <>
+        <div className="container">
+            <div className="flex justify-center items-center">
+                    <div className="grid grid-cols-5 gap-4">
+                        {listPokemon?.map((value, index) => (
+                            <div>
+                                <Card key={index} style={{height: '22vh', width: '15vw'}}>
+                                    <Typography gutterBottom variant="h5" component="div">Pokemon</Typography>
+                                    <Typography gutterBottom variant="p" component="div" >Nome: {value.name}</Typography>
+
+                                    {value?.name === pokemon?.name ? 
+                                        pokemon?.abilities.map((ability, index) => (
+                                            <Typography gutterBottom variant="p" component="div" key={index}>{index+1+"°"} Habilidade: {ability.ability.name}</Typography>
+                                            ))
+                                    : null}
+                                    <Button className="m-4" onClick={() => selectPokemon(value.name)} variant="contained">
+                                        Ver mais
+                                    </Button>
+                                </Card>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="flex justify-around m-4">
+                    {next > 0 && (
+                        <Button className="m-4" onClick={() => setNext(next > 0 && next - 20)} variant="contained">
+                        Anterior
+                        </Button>
+                    )}
+                    <Button className="m-4" onClick={() => setNext(next + 20)} variant="contained">
+                        Proximo
+                    </Button>
                 </div>
             </div>
-            <div className="flex justify-around m-4">
-                <Button className="m-4" onClick={() => setNext(next > 0 && next - 20)} variant="contained">
-                    Anterior
-                </Button>
-                <Button className="m-4" onClick={() => setNext(next + 20)} variant="contained">
-                    Proximo
-                </Button>
-            </div>
-        </div></>
+        </>
     );
        
 }
