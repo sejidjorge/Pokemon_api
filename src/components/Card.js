3a911557e85685cbb26c590e5624f43b617293e2
Jorge <sejidjorge@gmail.com>
2021-12-15 14:33:34 -0300

    ajustes estilos

--- src/components/Card.js
+++ src/components/Card.js
@@ -21,8 +21,11 @@ export default function UserCard(){
             <div className="flex justify-center mt-4">
                     <div className="grid grid-cols-1 gap-1">
                             <div>
+
+                            </div>
+                            <div>
                                 <Card className="min-h-min">
-                                    <Typography className="font-bold capitalize" gutterBottom variant="h5" component="div">{pokemon?.name}</Typography>
+                                    <Typography className="capitalize" gutterBottom variant="h5" component="div">{pokemon?.name}</Typography>
                                     <div className="felx grid grid-cols-2 gap-3">
                                         <div className="ml-2">
                                             <Typography gutterBottom className="font-bold capitalize" variant="p" component="div">Tipo:</Typography>
@@ -39,16 +42,16 @@ export default function UserCard(){
                                                     ))
                                             }
                                         </div>
-                                        <div className="ml-2">
-                                            <Typography gutterBottom className="font-bold capitalize" variant="p" component="div">Estatisticas:</Typography>
-                                                <div className="justify-between grid grid-cols-2 gap-2">
-                                                {pokemon?.stats.map((stat, index) => (
-                                                    <Typography className="text-sm text-left capitalize" variant="p" component="div" key={index}>
-                                                    <span className="font-bold">{stat.stat.name}</span>: {stat.base_stat}
-                                                </Typography>
-                                                ) )}
-                                                </div>
-                                        </div>
+                                    </div>
+                                    <div className="ml-2">
+                                        <Typography gutterBottom className="font-bold capitalize" variant="p" component="div">Estatisticas:</Typography>
+                                            <div className="justify-between grid grid-cols-2 gap-2">
+                                            {pokemon?.stats.map((stat, index) => (
+                                                <Typography className="text-sm text-left capitalize" variant="p" component="div" key={index}>
+                                                <span className="font-bold">{stat.stat.name}</span>: {stat.base_stat}
+                                            </Typography>
+                                            ) )}
+                                            </div>
                                     </div>
                                     {
                                         <CardMedia
